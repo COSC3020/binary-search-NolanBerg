58052bee1453c3236ec959bcc67fde1a67db83f5
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-30 20:48:39 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,9 @@ function binarySearch(numList, element, low, high) {
     if (high >= low) {
         var mid = Math.floor((low + high) / 2);
         if (numList[mid] === element) {
+            while (mid > 0 && numList[mid - 1] === element) {
+                mid--;
+            }
             return mid;
         } else if (numList[mid] > element) {
             return binarySearch(numList, element, low, mid - 1);
