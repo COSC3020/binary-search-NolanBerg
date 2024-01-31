d85daff9fb41e97b162c4d1df86d6c2d3db4cdb7
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-31 01:36:39 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,15 +1,13 @@
-const fs = require('fs');
-const jsc = require('jsverify');
+const assert = require('assert');
+const { binarySearch } = require('./code');
 
-eval(fs.readFileSync('code.js') + '');
+assert.strictEqual(binarySearch([], 10), -1, 'Empty array should return -1');
 
-const testSearch = jsc.forall('array nat', function (arr) {
-    if (arr.length > 0) {
-       arr.sort(function (a, b) { return a - b; });
-        return binarySearch(arr, arr[0]) === 0;
-    } else {
-        return binarySearch(arr, 10) === -1;
-    }
-});
+assert.strictEqual(binarySearch([5], 10), -1, 'Single-element array should return -1');
 
-jsc.assert(testSearch);
+assert.strictEqual(binarySearch([10], 10), 0, 'Single-element array should return 0');
+
+assert.strictEqual(binarySearch([1, 2, 3, 3, 3, 4, 5], 3), 2, 'Should return the index of the first occurrence');
+
+
+console.log('All tests passed successfully!');
