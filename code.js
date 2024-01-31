496ffc66ddb9df19001599961e235ba7db845724
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-31 00:28:38 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,12 @@
 function binarySearch(numList, element, low = 0, high = numList.length - 1) {
+    let firstOccurrence = -1;
+
     while (low <= high) {
         const mid = Math.floor((low + high) / 2);
 
         if (numList[mid] === element) {
-            let firstOccurrence = mid;
-            while (firstOccurrence > 0 && numList[firstOccurrence - 1] === element) {
-                firstOccurrence--;
-            }
-            return firstOccurrence;
+            firstOccurrence = mid;
+            high = mid - 1;
         } else if (numList[mid] < element) {
             low = mid + 1;
         } else {
@@ -15,7 +14,7 @@ function binarySearch(numList, element, low = 0, high = numList.length - 1) {
         }
     }
 
-    return -1; 
+    return firstOccurrence;
 }
 
 if (typeof module !== 'undefined' && module.exports) {
