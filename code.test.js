301e653e7f009fe53065db203eb0f65b4c6a7e9f
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-31 00:40:54 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js') + '');
 
-const testSearch =
-    jsc.forall("array nat", function(arr) {
-        if(arr.length > 0) {
-            arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0;
-        } else {
-            return binarySearch(arr, "foo") === -1;
-        }
-    });
+const testSearch = jsc.forall('array nat', function (arr) {
+    if (arr.length > 0) {
+       arr.sort(function (a, b) { return a - b; });
+        return binarySearch(arr, arr[0]) === 0;
+    } else {
+        return binarySearch(arr, 10) === -1;
+    }
+});
 
 jsc.assert(testSearch);
