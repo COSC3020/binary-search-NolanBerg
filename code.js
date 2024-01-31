1f765d194fff467bc3cbdc26116fa61081bd4e31
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-01-30 20:35:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function binarySearch(list, element) {
-    return -1;
+function binarySearch(numList, element, low, high) {
+    if (high >= low) {
+        var mid = Math.floor((low + high) / 2);
+        if (numList[mid] === element) {
+            return mid;
+        } else if (numList[mid] > element) {
+            return binarySearch(numList, element, low, mid - 1);
+        } else {
+            return binarySearch(numList, element, mid + 1, high);
+        }
+    } else {
+        return -1;
+    }
 }
+
+module.exports = {
+    binarySearch
+};
